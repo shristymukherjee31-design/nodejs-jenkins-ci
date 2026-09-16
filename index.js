42b1fb421f146936b8c345ae4eb014d6d7d3e86a
Shristy <shristymukherjee31@gmail.com>
2026-09-16 10:57:30 +0000

    websook test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node form Shristy!\n'
+  const msg = 'Hello Node form Shristy, guided by Sahil sir!\n'
   res.end(msg);
 });
 
